a010d590bc2a1b47aa7eea3a88ea014f2e14b4f3
Rohit Kumar Mahaseth <rohitmahaseth@gmail.com>
2022-06-29 12:39:41 +0530

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 // type rafce to get starting syntax
 import React from 'react'
 import Restraunt from "./component/basics/Restraunt.js"
+// import UseState from "./component/Hooks/useState"
+// import UseEffect from "./component/Hooks/useEffect"
+// import UseReducer from "./component/Hooks/useReducer"
+
 
 const App = () => {
   return (
